948e6638da338c82bacac9132b46e6971e6db372
zuishuainanhai <1705902650@qq.com>
2021-11-21 13:17:54 +0800

    配置要排除的打包的包

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,43 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // port = 9528 npm run dev OR npm run dev --port = 9528
 // 短路 设置开发服务端口 node中的process进程 用webpack注入到js
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+// 把排除的大体积文件=》使用cdn方式引入（上线生产环境生效,开发环境不排除
+/**
+      * externals 对象属性解析：
+      * '包名' : '模块内置对象'
+    */
+//  准备排除的配置
+let externals = {}
+// 准备cdn文件
+let cdn = {
+  css: [],
+  js: []
+}
+const isEnv_prod = process.env.NODE_ENV === 'productions' // 生产环境
+if (isEnv_prod) {
+  // 排除
+  // 1. 减少应用打包出来的包体积
+  // 2. 加快静态资源的访问
+  // 3. 利用浏览器缓存，不会变动的文件长期缓存
 
+  externals = {
+    // 值是cdn实际引入的包的全局变量名=====排除key的包，要用cdn注入全局变量名对应的包
+    'vue': 'Vue',
+    'element': 'ELEMENT',
+    'xlsx': 'XLSX'
+  }
+  // cdn
+  cdn = {
+    css: ['https://cdn.jsdelivr.net/npm/element-ui@2.13.2/lib/theme-chalk/index.css'],
+    js: [
+      // vue must at first!
+      'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+      // element-ui js
+      'https://cdn.jsdelivr.net/npm/element-ui@2.13.2/lib/index.js',
+      // xlsx
+      'https://cdn.jsdelivr.net/npm/xlsx@0.16.6/dist/xlsx.full.min.js']
+  }
+}
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
   /**
@@ -50,16 +86,19 @@ module.exports = {
     }
     // before: require('./mock/mock-server.js')
   },
+  // 打包执行
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     name: name,
+    externals,
     resolve: {
       alias: {
         '@': resolve('src')
       }
     }
   },
+  // 打包时候才会执行的插件配置
   chainWebpack (config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
     config.plugin('preload').tap(() => [
@@ -71,7 +110,6 @@ module.exports = {
         include: 'initial'
       }
     ])
-
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
 
